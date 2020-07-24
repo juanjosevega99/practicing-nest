a3334c5ef80dedaf095510d6dabcf85b36f044af
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-24 15:26:18 -0500

    [ADD] connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,15 @@ import { UsersModule } from './users/users.module';
 import { ProblemsController } from './problems/problems.controller';
 import { ProblemsService } from './problems/problems.service';
 import { ProblemsModule } from './problems/problems.module';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [AgentsModule, UsersModule, ProblemsModule],
+  imports: [
+    AgentsModule, 
+    UsersModule,
+    ProblemsModule,
+    MongooseModule.forRoot('mongodb+srv://db_user_platzivideos:rompiendola.desde.l9l9@cluster0-f0pdk.mongodb.net/alexa?retryWrites=true&w=majority')
+  ],
   controllers: [AppController, AgentsController, UsersController, ProblemsController],
   providers: [AppService, AgentsService, UsersService, ProblemsService],
 })
