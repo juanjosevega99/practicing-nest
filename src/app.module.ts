315a78b16130a0a07a18035c47aca6f2979f3d1a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-24 07:02:52 -0500

    [ADD] controller, service, module agents

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AgentsController } from './agents/agents.controller';
+import { AgentsService } from './agents/agents.service';
+import { AgentsModule } from './agents/agents.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AgentsModule],
+  controllers: [AppController, AgentsController],
+  providers: [AppService, AgentsService],
 })
 export class AppModule {}
